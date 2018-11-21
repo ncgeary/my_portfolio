d53b1c1c461725bd616329e391c8e35524473981
Nicholas Geary <ncgeary@buffalo.edu>
2018-11-21 17:08:32 -0500

    11/21/2018

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -9,11 +9,14 @@ class Jumbotron extends Component{
           <h1 className="display-4">Nick Geary</h1>
           <p className="lead">Engineer | Developer | Problem Solver</p>
           <p className="social">
-            
-
-             <a href="https://www.linkedin.com/in/nicholasgeary/" rel='noreferrer noopener' target="_blank"><i className="fab fa-linkedin-in"></i>  Linkedin</a>
-
-             <a href="https://github.com/ncgeary" rel='noreferrer noopener' target="_blank"><i className="fab fa-github"></i>  Github</a>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/nicholasgeary/" rel='noreferrer noopener' target="_blank"><i className="fab fa-linkedin-in"></i>  Linkedin</a>
+              </li>
+              <li>
+                <a href="https://github.com/ncgeary" rel='noreferrer noopener' target="_blank"><i className="fab fa-github"></i>  Github</a>
+              </li>
+            </ul>
           </p>
         </div>
       </div>
