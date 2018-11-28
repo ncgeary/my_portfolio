2c1730bbfc788f46e445c37335f60e03c2e55cf4
Nicholas Geary <ncgeary@buffalo.edu>
2018-11-27 22:14:24 -0500

    11/27/2018

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,44 +7,44 @@ class Navbar extends Component{
   render(){
     return (
 
-    <Navbar className="navbar-fixed-top">
-      <NavItem><Scrollchor to="/" className="nav-link">Home</Scrollchor></NavItem>
+    // <Navbar className="navbar-fixed-top">
+    //   <Scrollchor to="/" className="nav-link">Home</Scrollchor>
+    //
+    //   <Scrollchor to="#Skills" className="nav-link">Skills</Scrollchor>
+    //
+    //   <Scrollchor to="#Project" className="nav-link">Projects</Scrollchor>
+    //
+    //   <Scrollchor to="#History" className="nav-link">History</Scrollchor>
+    //
+    //   <Scrollchor to="/Contact" className="nav-link">Contact Me</Scrollchor>
+    // </Navbar>
 
-      <NavItem><Scrollchor to="#Skills" className="nav-link">Skills</Scrollchor></NavItem>
+      <nav className="navbar navbar-expand-lg navbar-fixed-top">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <NavItem><Scrollchor to="#Project" className="nav-link">Projects</Scrollchor></NavItem>
+        <div className="navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
 
-      <NavItem><Scrollchor to="#History" className="nav-link">History</Scrollchor></NavItem>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/Project">Projects</Link>
+            </li>
 
-    <NavItem><Scrollchor to="/Contact" className="nav-link">Contact Me</Scrollchor></NavItem>
-    </Navbar>
-
-      // <nav className="navbar navbar-expand-lg navbar-fixed-top">
-      //   <Link className="navbar-brand" to="/">Home</Link>
-      //   <button
-      //     className="navbar-toggler"
-      //     type="button"
-      //     data-toggle="collapse"
-      //     data-target="#navbarSupportedContent"
-      //     aria-controls="navbarSupportedContent"
-      //     aria-expanded="false"
-      //     aria-label="Toggle navigation">
-      //     <span className="navbar-toggler-icon"></span>
-      //   </button>
-      //
-      //   <div className="navbar-collapse" id="navbarSupportedContent">
-      //     <ul className="navbar-nav">
-      //
-      //       <li className="nav-item active">
-      //         <Link className="nav-link" to="/Project">Projects</Link>
-      //       </li>
-      //
-      //       <li className="nav-item active">
-      //         <Link className="nav-link" to="/Contact">Contact Me</Link>
-      //       </li>
-      //     </ul>
-      //   </div>
-      // </nav>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/Contact">Contact Me</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
 
     );
